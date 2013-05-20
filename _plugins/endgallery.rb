696358e1e02d32a9f5082002dabfbb82c8f3c13c
Josh Davis <josh@joshldavis.com>
2013-05-20 13:23:46 -0700

    Remove plugins in favor of raw HTML

--- _plugins/endgallery.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-    class EndGalleryTag < Liquid::Tag
-        def render(context)
-            '</div>'
-        end
-    end
-end
-
-Liquid::Template.register_tag('endgallery', Jekyll::EndGalleryTag)
