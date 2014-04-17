fbc725c09c9b8fef58f68a8e5fc7a8394cab7087
Josh Davis <josh@joshldavis.com>
2014-04-16 19:37:33 -0500

    Hide when in mobile for now

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@
     var $button = $('button.nav'),
         $nav = $('.side ul');
 
-    $('.js').show();
-    $('.no-js').hide();
-
     $button.on('click', function () {
         $nav.slideToggle();
     });
